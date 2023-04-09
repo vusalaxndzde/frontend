c9f43d6eea89fac68b3b2486cff6e79939d7331f
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-09 06:01:30 +0400

    javascript

---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+let firstName = "Vusal"
+let lastName = "Akhundzada"
+let age = 20
+let country = "Azerbaijan"
+let isMarried = false
+
+console.log(firstName, lastName, age, country, isMarried)
+
+const pi = 3.14
+pi = 10 // exception
+console.log(pi) 
