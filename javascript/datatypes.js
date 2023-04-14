28a5d6a7481966f39e7f3c263061e512e01d96f4
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-14 23:41:11 +0400

    changes

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -5,3 +5,18 @@ console.log(typeof 5.5)
 console.log(typeof true)
 console.log(typeof null)
 console.log(typeof undefined)
+
+
+let arr = [1,2,3,4,5]
+
+let s = "Hello"
+let s2 = "Hello"
+console.log(s === s2)
+
+let obj = {
+    "name" : "Vusal",
+    "surname" : "Akhundzada",
+    "age" : 20
+}
+
+console.log(obj)
