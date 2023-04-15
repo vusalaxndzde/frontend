51d4d39772df4ce9a62adfd1e0f7b483afdbbee3
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-16 01:36:47 +0400

    changes

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -1,4 +1,5 @@
 let a = 5
+// let a = prompt("number: ")
 if (a > 0) {
     console.log(`${a} is positive`)
 } else if (a < 0) {
@@ -7,4 +8,28 @@ if (a > 0) {
     console.log(`num is 0`)
 } else {
     console.log(`num is not number`)
+}
+
+
+let userInput = prompt("Operation:")
+userInput = userInput.split("")
+let num1 = parseFloat(userInput[0]) 
+let num2 = parseFloat(userInput[2]) 
+let operation = userInput[1]
+
+switch(operation) {
+    case '+':
+        alert(num1 + num2)
+        break
+    case '-':
+        alert(num1 - num2)
+        break
+    case '*':
+        alert(num1 * num2)
+        break
+    case '/':
+        alert(num1 / num2)
+        break
+    default:
+        alert("Error") 
 }
