a205e4d684d43e2f98ead1052bc2b645c20e608d
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-16 02:51:05 +0400

    changes

---
+++ javascript/arrays.js
@@ -0,0 +1,21 @@
+let arr1 = new Array()
+arr1 = Array()
+
+let arr = [1, 2, 3, 4, 5]
+console.log(arr)
+
+arr.push(6)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.unshift(0)
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+let index = arr.indexOf(3)
+arr.splice(index, 1, 0)
+console.log(arr)
