96c49ff8f774660347b5cf7c9677910e7594be69
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-16 01:08:47 +0400

    changes

---
+++ javascript/conditions.js
@@ -0,0 +1,10 @@
+let a = 5
+if (a > 0) {
+    console.log(`${a} is positive`)
+} else if (a < 0) {
+    console.log(`${a} is negative`)
+} else if (a == 0) {
+    console.log(`num is 0`)
+} else {
+    console.log(`num is not number`)
+}
