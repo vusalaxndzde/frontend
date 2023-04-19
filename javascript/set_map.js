d664d1a4a2bf4de648ce3213c53d05b01ee3a68a
vusalaxndzde <vusal.axndzde@gmail.com>
2023-04-20 02:12:36 +0400

    changes

--- javascript/set_map.js
+++ javascript/set_map.js
@@ -21,3 +21,24 @@ console.log(set)
 console.log(set.has(2))
 set.delete(1)
 console.log(set)
+
+const a = new Set([1,2,3,4])
+const b = new Set([3,4,5,6])
+const c = new Set([...a, ...b])
+console.log(c)
+
+
+const map = new Map();
+map.set("name", "vusal")
+console.log(map)
+
+countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo'],
+]
+const countriesMap = new Map(countries)
+console.log(countriesMap)
+for (const [country, city] of countriesMap) {
+    console.log(country, city)
+}
