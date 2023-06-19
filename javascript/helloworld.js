ed501229a32275d27750636b34386048a4f99585
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-19 17:41:32 +0400

    changes

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -4,3 +4,13 @@ console.log(metn)
 let element = document.createElement('p')
 element.textContent = 'hello'
 console.log(element)
+
+document.body.appendChild(element)
+
+let silinecek = document.getElementById('salam-2')
+console.log(silinecek)
+document.body.removeChild(silinecek)
+
+let link = document.querySelector('a')
+link.href = '##'
+console.log(link)
