d2bb9ed163c0cf550f61968da534894fb6a30a58
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-20 17:09:43 +0400

    changes

--- javascript/task/index.js
+++ javascript/task/index.js
@@ -3,3 +3,11 @@ function deleteLi(element) {
     let ul = document.querySelector('ul')
     ul.removeChild(element)
 }
+
+function createUser() {
+    let input = document.querySelector('input')
+    let ul = document.querySelector('ul')
+    let li = document.createElement('li')
+    li.textContent = input.value
+    ul.appendChild(li)
+}
