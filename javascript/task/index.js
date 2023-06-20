a132357afe85e44c143f3a924adc820587e2a925
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-20 17:41:18 +0400

    changes

--- javascript/task/index.js
+++ javascript/task/index.js
@@ -11,3 +11,17 @@ function createUser() {
     li.textContent = input.value
     ul.appendChild(li)
 }
+
+function tekrariSil(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                arr.splice(j, 1)
+                j--;
+            }
+        }
+    }
+    console.log(arr)
+}
+
+tekrariSil([1,1,2,3,4,3,2,6])
