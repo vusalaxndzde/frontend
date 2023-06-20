7988e2ba44ed64d18c4cdd52a66a5ec92b3f8a6e
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-20 18:29:47 +0400

    changes

--- javascript/task/index.js
+++ javascript/task/index.js
@@ -17,7 +17,7 @@ function tekrariSil(arr) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] == arr[j]) {
                 arr.splice(j, 1)
-                j--;
+                j--
             }
         }
     }
@@ -25,3 +25,23 @@ function tekrariSil(arr) {
 }
 
 tekrariSil([1,1,2,3,4,3,2,6])
+
+
+let arr = [
+    {ad : "vusal", giris : 34, bal : 18},
+    {ad : "vusal", giris : 45, bal : 20},
+    {ad : "vusal", giris : 34, bal : 16},
+    {ad : "vusal", giris : 32, bal : 17}
+]
+
+function check(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].bal < 17 || (arr[i].bal + arr[i].giris) < 51) {
+            arr.splice(i, 1)
+            i--
+        }
+    }
+    console.log(arr)
+}
+
+check(arr)
